b7418b6f7a0ed603edcf38a064e377ef3819135d
RowiPxl <rowi1912@hotmail.com>
2022-03-28 15:42:09 +0200

    Added generate method for aes keys and iv's

---
+++ ClassLibrary1/GenerateAesKey.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public static class GenerateAesKey
+    {
+        public static byte[] AesKey()
+        {
+            AesManaged aes = new AesManaged();
+            aes.GenerateKey();
+            return aes.Key;
+        }
+        public static byte[] AesIv()
+        {
+            AesManaged aes = new AesManaged();
+            aes.GenerateKey();
+            return aes.IV;
+        }
+
+
+        public static string AesKeyString()
+        {
+            
+            return Convert.ToBase64String(AesKey());
+        }
+
+        public static string AesIvString()
+        {
+            return Convert.ToBase64String(AesIv());
+        }
+    }
+}
