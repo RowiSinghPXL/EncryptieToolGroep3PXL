6becc4335a780f47031373b6884ead7d8d436714
RowiPxl <rowi1912@hotmail.com>
2022-04-14 00:59:22 +0200

    Added random number generator for generating cred

---
+++ ClassLibrary1/RandomGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public static class RandomGenerator
+    {
+        public static byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+    }
+}
