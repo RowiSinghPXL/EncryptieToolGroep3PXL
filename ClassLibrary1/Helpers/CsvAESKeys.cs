c38dea1d507f66a82b9287bbfc983cea647a9e77
RowiPxl <rowi1912@hotmail.com>
2022-04-13 20:56:45 +0200

    User can now generate key + iv, generated cred are now being stored

---
+++ ClassLibrary1/Helpers/CsvAESKeys.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using ClassLibrary1.Model;
+using CsvHelper;
+
+namespace ClassLibrary1.Helpers
+{
+    public static class CsvAESKeys
+    {
+        public static string FileKeysAES { get; } = @"AesKey.csv";
+        public static string FullPath { get; } = Directory.Path + @"\" + FileKeysAES;
+        public static void InitAESKeyFile()
+        {
+            string headerFile = "Key;Iv";
+            //var csvPath = Directory.Path + @"\" + FileKeysAES;
+
+
+            //var records = new List<AESKey> { };
+
+            try
+            {
+                if (!File.Exists(FullPath)) { 
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(FullPath, true))
+                    {
+                        file.WriteLine(headerFile);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Fout bij het initialiseren van het csv bestand", ex);
+            }
+        }
+
+        public static void AESKeyaddRecord(string key, string iv)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(FullPath, true))
+                {
+                    file.WriteLine(key + ";" + iv);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new ApplicationException("Fout bij het opslaan van de key en iv", ex);
+            }
+        }
+
+
+
+    }
+}
