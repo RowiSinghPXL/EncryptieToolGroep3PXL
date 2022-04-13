4bbdc0c012f0b96918a3750b8080a02d97f052d0
RowiPxl <rowi1912@hotmail.com>
2022-04-13 19:45:37 +0200

    Program creates aeskeyfile

---
+++ ClassLibrary1/Helpers/Csv.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using ClassLibrary1.Model;
+using CsvHelper;
+
+namespace ClassLibrary1.Helpers
+{
+    public static class Csv
+    {
+        public static string FileKeysAES { get; } = @"AesKey.csv";
+        public static void InitAESKeyFile()
+        {
+            string headerFile = "Key, Iv";
+            var csvPath = Directory.Path + @"\" + FileKeysAES;
+
+
+            //var records = new List<AESKey> { };
+
+            using (var writer = new StreamWriter(csvPath))
+            using (var csv = new CsvWriter(writer, CultureInfo.CurrentCulture))
+            {
+                csv.WriteHeader<AESKey>();
+               
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/Helpers/Directory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ClassLibrary1.Helpers
+{
+    public static class Directory
+    {
+        public static string Path { get;} = @"c:\EncryptieToolGroep3";
+       
+
+
+        public static void InitDirectory()
+        {
+            //string path = @"c:\EncryptieToolGroep3";
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (System.IO.Directory.Exists(Path))
+                {
+                    Console.WriteLine("That path exists already.");
+                    return;
+                }
+
+                // Try to create the directory.
+                DirectoryInfo di = System.IO.Directory.CreateDirectory(Path);
+                Console.WriteLine("The directory was created successfully at {0}.", System.IO.Directory.GetCreationTime(Path));
+
+                // Delete the directory.
+                //di.Delete();
+                //Console.WriteLine("The directory was deleted successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            finally { }
+        }
+
+        
+
+    }
+}
